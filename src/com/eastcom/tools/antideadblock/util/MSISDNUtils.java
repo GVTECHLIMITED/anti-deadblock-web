0120d7a0c029e3fe7f82e12d4a9875d8e76bdf2e
Md. Rajulul Islam (Alex) <46351079+GVTECHLIMITED@users.noreply.github.com>
2022-09-02 12:25:37 +0530

    Update MSISDNUtils.java

--- src/com/eastcom/tools/antideadblock/util/MSISDNUtils.java
+++ src/com/eastcom/tools/antideadblock/util/MSISDNUtils.java
@@ -1,38 +1,34 @@
-/**
- * MSISDNUtils.java was created on 2013年7月28日 下午3:04:56
- *
- * Copyright (c) 2013 EASTCOM Software Technology Co., Ltd. All rights reserved.
- */
-package com.eastcom.tools.antideadblock.util;
-
-/**
- * @author sqwen
- */
-public class MSISDNUtils {
-
-    private MSISDNUtils() {}
-
-    /**
-     * 将号码进行标准化
-     * @param origMSISDN 原始号码
-     * @return 标准化后的号码
-     */
-    public static final String sanitizeToString(String origMSISDN) {
-        origMSISDN = origMSISDN.trim();
-        return origMSISDN.startsWith("86") ? origMSISDN : "86" + origMSISDN;
-    }
-
-    /**
-     * 将号码转换为长整形
-     * @param origMSISDN 原始号码
-     * @return 标准化为长整形的号码
-     */
-    public static final long sanitizeToLong(String origMSISDN) {
-        return Long.parseLong(sanitizeToString(origMSISDN));
-    }
-
-    public static final boolean isValid(String msisdn) {
-        return msisdn.length() == 11 || msisdn.length() == 13 && msisdn.startsWith("86");
-    }
-
+public final class MsisdnUtils {
+  public static final String MSISDN = "^(\\+?63?|0)[89]([0-9]{9})$";
+  public static final Integer MIN_LENGTH_SHORT = 10;
+  private MsisdnUtils() {
+      // hiding constructor
+  }
+  public static String convertMsisdnWithPlus(String msisdn) {
+      if(isValidMsisdn(msisdn)) {
+          return convertMsisdn(msisdn, "+63");
+      }
+      return null;
+  }
+  public static String convertMsisdn(String msisdn, String prefix) {
+      if(prefix == null) {
+          throw new IllegalArgumentException("Msisdn Prefix is invalid.");
+      }
+      if(isValidMsisdn(msisdn)) {
+          return prefix + msisdn.substring(msisdn.length() - MIN_LENGTH_SHORT);
+      }
+      return null;
+  }
+  public static boolean isValidMsisdn(String msisdn) {
+      if(StringUtils.isBlank(msisdn)){
+          return false;
+      }
+      return Pattern.compile(MSISDN).matcher(msisdn).matches();
+  }
 }
+
+MsisdnUtils Notes
+* MSISDN = Mobile Number
+* Valid Mobile Number formats samples: +639191234567, 639191234567, 09191234567
+* Convert methods returns the converted mobile number or null if mobile number is not valid
+* Validation methods return true if mobile number is valid, false otherwise.
